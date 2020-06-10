f7b18280d37ff8908458bf1f0cf3ed768a6768ea
bazegie07 <bazegie_07@yahoo.com>
2020-06-09 17:22:58 -0700

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
+
+  siteMetadata:{
+    title: "Gatsby Tutorial",
+    description: "some random description",
+    author: "@bazegie_07",
+    data: ["item1", "item2"],
+    person: {name: "stephen", age: "29"},
+  },
   plugins: [`gatsby-plugin-styled-components`],
 }
+
